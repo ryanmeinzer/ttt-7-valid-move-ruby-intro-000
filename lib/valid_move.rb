9f981971e5658767d9ef0af38ac3276b92e09060
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-03-15 23:19:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,8 @@
 def valid_move?(board, index)
   if index.between?(0, 8)
     return true
-  else position_taken?(board, index)
+  elsif position_taken?(board, index)
+    return false
   end
 end
 
